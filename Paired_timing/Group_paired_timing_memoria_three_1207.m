dd6ce0a5a445957163e48e13ddff5ac684ad997e
ying.fang@outlook.com <Elke2008>
2018-12-07 08:29:44 -0800

    paired_timing

--- Paired_timing/Group_paired_timing_memoria_three_1207.m
+++ Paired_timing/Group_paired_timing_memoria_three_1207.m
@@ -7,13 +7,13 @@
 % 4. save data
 
 clear all;clc;
-project_name = 'MMR';
+project_name = 'Memoria';
 
 regions = {'Hippocampus';'PMC';'mPFC'};
 sbj_names ={'S18_119_AG';'S18_130_RH';'S18_131_L';'S18_131_R'};%;'S12_33_DA'};%};%;%;'S13_47_JT2'};%};%;'S18_125'
 locktype ='stim';
-datapath_sot= '/Volumes/Ying_SEEG/Data_lbcn/Results/MMR/Group/Stats';
-datapath_rol='/Volumes/Ying_SEEG/Data_lbcn/Results/MMR/Group/ROL'; % mmr permutation results path, used for selected sig channel
+datapath_sot= '/Volumes/Ying_SEEG/Data_lbcn/Results/Memoria/Group/Stats';
+datapath_rol='/Volumes/Ying_SEEG/Data_lbcn/Results/Memoria/Group/ROL'; % Memoria permutation results path, used for selected sig channel
 regions={'Hippocampus','PMC','mPFC'};%
 
 freq_band='HFB';
@@ -52,19 +52,19 @@ for ri=1:length(regions)
         enum=elecs{ri};
         filename1=dir(fullfile(datapath_sot,[sbj_name,'*',regions{ri},'.mat']));
         load(fullfile(datapath_sot,filename1.name));
-        s_ele=intersect(find([el_stats.sig_hfb_autobio]>0),find([el_stats.sig_hfb_automath]>0));
-        sot_ele= intersect(find(el_stats.hfb_start>=0.2),find(el_stats.hfb_start<=1.5));
-        rolmean= intersect(find(ROL.mean>=0.2),find(ROL.mean<=1.5));
-        rolmedian= intersect(find(ROL.median>=0.2),find(ROL.median<=1.5));
-        rolfitmean= intersect(find(ROL.fitmean>=0.2),find(ROL.fitmean<=1.5));
-        rolfitmedian= intersect(find(ROL.fitmedian>=0.2),find(ROL.fitmedian<=1.5));
+        s_ele=find([el_stats.sig_hfb_autobio]>0);
+        sot_ele= intersect(find(el_stats.hfb_start>=0.2),find(el_stats.hfb_start<=7));
+        rolmean= intersect(find(ROL.mean>=0.2),find(ROL.mean<=7));
+        rolmedian= intersect(find(ROL.median>=0.2),find(ROL.median<=7));
+        rolfitmean= intersect(find(ROL.fitmean>=0.2),find(ROL.fitmean<=7));
+        rolfitmedian= intersect(find(ROL.fitmedian>=0.2),find(ROL.fitmedian<=7));
         
         data.(regions{ri}).sot=[data.(regions{ri}).sot;el_stats.hfb_start(sot_ele)];
-        data.(regions{ri}).rolmean=[data.(regions{ri}).rolmean;ROL.mean(intersect(sot_ele,rolmean))'];
-        data.(regions{ri}).rolmedian=[data.(regions{ri}).rolmedian;ROL.median(intersect(sot_ele,rolmedian))'];
+        data.(regions{ri}).rolmean=[data.(regions{ri}).rolmean;ROL.mean(intersect(sot_ele,rolmean))];
+        data.(regions{ri}).rolmedian=[data.(regions{ri}).rolmedian;ROL.median(intersect(sot_ele,rolmedian))];
         
-        data.(regions{ri}).rolfitmean=[data.(regions{ri}).rolfitmean;ROL.fitmean(intersect(sot_ele,rolfitmean))'];
-        data.(regions{ri}).rolfitmedian=[data.(regions{ri}).rolfitmedian;ROL.fitmedian(intersect(sot_ele,rolfitmedian))'];    
+        data.(regions{ri}).rolfitmean=[data.(regions{ri}).rolfitmean;ROL.fitmean(intersect(sot_ele,rolfitmean))];
+        data.(regions{ri}).rolfitmedian=[data.(regions{ri}).rolfitmedian;ROL.fitmedian(intersect(sot_ele,rolfitmedian))];    
         
     end
 end
@@ -94,6 +94,6 @@ for i=1:length(cate)
 
 
 
-save ([datapath_sot,'/pairdata_MMR_three_1207.mat'],'data','sdata');
+save ([datapath_sot,'/pairdata_Memoria_three_1207.mat'],'data','sdata');
 
 %
